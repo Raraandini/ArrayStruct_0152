0ee893c3f6d4cfa786af850da3d35c584c69621f
Raraandinii <raraandinib@gmail.com>
2025-07-09 16:14:58 +0700

    tambahkan input data mahasiswa dari pengguna

--- struct.cpp
+++ struct.cpp
@@ -8,3 +8,18 @@ struct mahasiswa
     string alamat;
 };
 
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim = "20220140102";
+    mhs.nama = "Abra Yudhistira";
+    mhs.alamat = "Kasihan";
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan NAMA = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat = ";
+    cin >> mhs.alamat;
+
+}
